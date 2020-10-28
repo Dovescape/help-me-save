8bce45b503c6d4a1bb75e24ab93a2a1516d65f50
Nick <n.j.neutkens@gmail.com>
2020-10-28 08:01:56 -0500

    initial upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="header" id="header">
+        <h1>Welcome! What would you like to save for?</h1>
+        <div>
+          <button className="button">Let's Save!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
